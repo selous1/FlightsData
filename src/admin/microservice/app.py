ed708d5ccc5b2654b47a2cfd1294def6611d3f87
Santiago Benites <santi.mbenites@gmail.com>
2023-04-18 00:13:57 +0100

    Added kubernetes deployment to admin use case

--- src/admin/microservice/app.py
+++ src/admin/microservice/app.py
@@ -1,14 +1,18 @@
-import json, os, connexion, re
+import json
+import os
+import connexion
+import re
 from flask import Flask, request, jsonify
 from google.cloud import bigquery
 from google.oauth2 import service_account
 from flight import Flight  # noqa: E501
 
 # BigQuery client setup
-credentials = service_account.Credentials.from_service_account_file("cnproject-381016-a92327017fa2.json")
+credentials = service_account.Credentials.from_service_account_file("cnproject-381016-3aa6da06c093.json")
 client = bigquery.Client(credentials=credentials)
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=['GET'])
 def root():  # noqa: E501
@@ -17,6 +21,7 @@ def root():  # noqa: E501
         status=200,
     )
     return response
+
 
 @app.route("/admin/flights", methods=['POST'])
 def post_flight():  # noqa: E501
@@ -31,36 +36,36 @@ def post_flight():  # noqa: E501
     """
 
     body = request.json
-    #if connexion.request.is_json:
+    # if connexion.request.is_json:
     #    body = Flight.from_dict(connexion.request.get_json())  # noqa: E501
 
     flightDict = flatten_dict(body)
-    #Columns = 'FlightDate,Flight_Number_Operating_Airline,AirTime,Cancelled,Diverted,Tail_Number,Airline,OriginAirportID,OriginAirportSeqID,DepTime,DepDelayMinutes,DestAirportID,DestAirportSeqID,ArrTime,ArrDelayMinutes'
-    
-    ColumnsTranslation ={
-        'flightDate':'FlightDate',
-        'Airline':'Flight_Number_Operating_Airline',
-        'flightDuration':'AirTime',
-        'cancelled':'Cancelled',
-        'diverted':'Diverted',
-        'tailNumber':'Tail_Number',
-        'airlineCode':'Airline',
-        'departure_actual':'DepTime',
-        'departure_airportId':'OriginAirportID',
-        'departure_delay':'DepDelay',
-        'departure_scheduled':'CRSDepTime',
-        'arrival_actual':'ArrTime',
-        'arrival_airportId':'DestAirportID',
-        'arrival_delay':'ArrDelay',
-        'arrival_scheduled':'CRSElapsedTime',
-        'flightNumber':'Flight_Number_Operating_Airline'
+    # Columns = 'FlightDate,Flight_Number_Operating_Airline,AirTime,Cancelled,Diverted,Tail_Number,Airline,OriginAirportID,OriginAirportSeqID,DepTime,DepDelayMinutes,DestAirportID,DestAirportSeqID,ArrTime,ArrDelayMinutes'
+
+    ColumnsTranslation = {
+        'flightDate': 'FlightDate',
+        'Airline': 'Flight_Number_Operating_Airline',
+        'flightDuration': 'AirTime',
+        'cancelled': 'Cancelled',
+        'diverted': 'Diverted',
+        'tailNumber': 'Tail_Number',
+        'airlineCode': 'Airline',
+        'departure_actual': 'DepTime',
+        'departure_airportId': 'OriginAirportID',
+        'departure_delay': 'DepDelay',
+        'departure_scheduled': 'CRSDepTime',
+        'arrival_actual': 'ArrTime',
+        'arrival_airportId': 'DestAirportID',
+        'arrival_delay': 'ArrDelay',
+        'arrival_scheduled': 'CRSElapsedTime',
+        'flightNumber': 'Flight_Number_Operating_Airline'
     }
 
     # Create Set part of the query
     Columns = []
     values = []
-    for (col,val) in list(zip(flightDict.keys(),flightDict.values())):
-        if re.search("\d{4}-\d{2}-\d{2}",str(val)):
+    for (col, val) in list(zip(flightDict.keys(), flightDict.values())):
+        if re.search("\d{4}-\d{2}-\d{2}", str(val)):
             Columns.append("FlightDate,")
             values.append(f"CAST('{val}' AS Date),")
         elif represents_int(val):
@@ -70,8 +75,9 @@ def post_flight():  # noqa: E501
             Columns.append(f"{ColumnsTranslation[col]},")
             values.append(f"'{val}',")
 
-    #Values = f"CAST('{values[0]}' AS Date),{values[1]},{values[2]},{values[3]},{values[4]},'{values[5]}','{values[6]}',{values[7]},{values[8]},{values[9]},{values[10]},{values[11]},{values[12]},{values[13]},{values[14]}"
-    query = "INSERT INTO %s ( %s ) VALUES ( %s );" % ('cnproject-381016.cn54392dataset.flight_table', ''.join(Columns)[:-1], ''.join(values)[:-1])
+    # Values = f"CAST('{values[0]}' AS Date),{values[1]},{values[2]},{values[3]},{values[4]},'{values[5]}','{values[6]}',{values[7]},{values[8]},{values[9]},{values[10]},{values[11]},{values[12]},{values[13]},{values[14]}"
+    query = "INSERT INTO %s ( %s ) VALUES ( %s );" % (
+        'cnproject-381016.cn54392dataset.flight_table', ''.join(Columns)[:-1], ''.join(values)[:-1])
 
     results = client.query(query)
 
@@ -82,6 +88,7 @@ def post_flight():  # noqa: E501
     )
 
     return response
+
 
 @app.route("/admin/flights/<flight_number>", methods=['DELETE'])
 def delete_flight(flight_number):  # noqa: E501
@@ -103,6 +110,7 @@ def delete_flight(flight_number):  # noqa: E501
 
     return resp
 
+
 @app.route("/admin/flights/<flight_number>", methods=['PUT'])
 def put_flight(flight_number):  # noqa: E501
     """Update an existing flight
@@ -118,34 +126,34 @@ def put_flight(flight_number):  # noqa: E501
     """
     body = request.json
 
-    #if connexion.request.is_json:
+    # if connexion.request.is_json:
     #    body = Flight.from_dict(connexion.request.get_json())  # noqa: E501
 
     # Turn body to dict of values and list of columns
     flightDict = flatten_dict(body)
-    ColumnsTranslation ={
-        'flightDate':'FlightDate',
-        'Airline':'Flight_Number_Operating_Airline',
-        'flightDuration':'AirTime',
-        'cancelled':'Cancelled',
-        'diverted':'Diverted',
-        'tailNumber':'Tail_Number',
-        'airlineCode':'Airline',
-        'departure_actual':'DepTime',
-        'departure_airportId':'OriginAirportID',
-        'departure_delay':'DepDelay',
-        'departure_scheduled':'CRSDepTime',
-        'arrival_actual':'ArrTime',
-        'arrival_airportId':'DestAirportID',
-        'arrival_delay':'ArrDelay',
-        'arrival_scheduled':'CRSElapsedTime',
-        'flightNumber':'Flight_Number_Operating_Airline'
+    ColumnsTranslation = {
+        'flightDate': 'FlightDate',
+        'Airline': 'Flight_Number_Operating_Airline',
+        'flightDuration': 'AirTime',
+        'cancelled': 'Cancelled',
+        'diverted': 'Diverted',
+        'tailNumber': 'Tail_Number',
+        'airlineCode': 'Airline',
+        'departure_actual': 'DepTime',
+        'departure_airportId': 'OriginAirportID',
+        'departure_delay': 'DepDelay',
+        'departure_scheduled': 'CRSDepTime',
+        'arrival_actual': 'ArrTime',
+        'arrival_airportId': 'DestAirportID',
+        'arrival_delay': 'ArrDelay',
+        'arrival_scheduled': 'CRSElapsedTime',
+        'flightNumber': 'Flight_Number_Operating_Airline'
     }
 
     # Create Set part of the query
     start = ""
-    for (col,val) in list(zip(flightDict.keys(),flightDict.values())):
-        if re.search("\d{4}-\d{2}-\d{2}",str(val)):
+    for (col, val) in list(zip(flightDict.keys(), flightDict.values())):
+        if re.search("\d{4}-\d{2}-\d{2}", str(val)):
             start += f"FlightDate = CAST('{val}' AS Date),"
         elif represents_int(val):
             start += f"{ColumnsTranslation[col]} = {val},"
@@ -162,24 +170,25 @@ def put_flight(flight_number):  # noqa: E501
         status=200,
         mimetype='application/json'
     )
-    
+
     return response
 
 
-
 def flatten_dict(dd, separator='_', prefix=''):
-    return { prefix + separator + k if prefix else k : v
-             for kk, vv in dd.items()
-             for k, v in flatten_dict(vv, separator, kk).items()
-             } if isinstance(dd, dict) else { prefix : dd }
+    return {prefix + separator + k if prefix else k: v
+            for kk, vv in dd.items()
+            for k, v in flatten_dict(vv, separator, kk).items()
+            } if isinstance(dd, dict) else {prefix: dd}
+
 
 def represents_int(s):
-        try: 
-            int(s)
-        except ValueError:
-            return False
-        else:
-            return True
+    try:
+        int(s)
+    except ValueError:
+        return False
+    else:
+        return True
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

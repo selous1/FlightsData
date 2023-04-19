ba52364adbcaa7ea36240d13eb542a4c4085b8dd
João Ferreira <jbrazf.01@gmail.com>
2023-04-19 23:41:39 +0100

    tests on pred ms

--- src/prediction/aws-test/src/main.rs
+++ src/prediction/aws-test/src/main.rs
@@ -8,10 +8,25 @@ use std::env;
 
 use aws_sdk_lambda::{
     config::{Builder, Credentials, Region},
+    primitives::Blob,
     Client,
 };
 
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
 const AWS_REGION: &str = "eu-north-1";
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AWSResponse {
+    body: String,
+    status_code: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Args {
+    name: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -25,15 +40,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Err(e) => println!("couldn't interpret {}: {}", "AWS_ACCESS_KEY_SECRET", e),
     }
 
+    let a = Args {
+        name: "help".to_string(),
+    };
+    let req_args = Blob::new(serde_json::to_string(&a)?);
+
     let client = get_aws_client(AWS_REGION)?;
 
     let res = client
         .invoke()
         .function_name("arn:aws:lambda:eu-north-1:621272430898:function:testfunction")
+        .payload(req_args)
         .send()
         .await?;
 
-    println!("res: {:?}", res);
+    let bd = res.payload.as_ref().unwrap().as_ref().to_vec();
+    let body = String::from_utf8_lossy(bd.as_slice());
+    let value: Value = serde_json::from_str(&body)?;
+
+    println!("res: {:?}", value);
 
     print!("its done!");
 
--- src/prediction/prediction-ms/src/main.rs
+++ src/prediction/prediction-ms/src/main.rs
@@ -1,11 +1,23 @@
 use actix_web::{get, http::StatusCode, web, App, Error, HttpResponse, HttpServer, Responder};
-use serde::Serialize;
+use aws_sdk_lambda::{
+    config::{Builder, Credentials, Region},
+    primitives::Blob,
+    Client,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::env;
 
 #[derive(Serialize)]
 struct PredictionResult {
     cancel_prob: f32,
     divert_prob: f32,
     expct_delay: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AWSArgs {
+    name: String,
 }
 
 #[actix_web::main]
@@ -21,7 +33,7 @@ async fn liveness() -> impl Responder {
     HttpResponse::Ok().body("Alive!")
 }
 
-#[get("/")] 
+#[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
@@ -34,7 +46,46 @@ async fn predict() -> Result<impl Responder, Error> {
         expct_delay: 0.0,
     };
 
-    // call prediction here
+    let client = get_aws_client("eu-north-1").unwrap();
 
-    Ok(web::Json(res))
+    let req_struct = AWSArgs {
+        name: "test".to_string(),
+    };
+
+    let req_args = Blob::new(serde_json::to_string(&req_struct)?);
+
+    let res = client
+        .invoke()
+        .function_name("arn:aws:lambda:eu-north-1:621272430898:function:testfunction")
+        .payload(req_args)
+        .send()
+        .await;
+
+    match res {
+        Ok(res) => {
+            let bd = res.payload.as_ref().unwrap().as_ref().to_vec();
+            let body = String::from_utf8_lossy(bd.as_slice());
+            let value: Value = serde_json::from_str(&body)?;
+
+            Ok(web::Json(value))
+        }
+        Err(e) => {
+            panic!("Error: {:#?}", e)
+        }
+    }
+}
+
+fn get_aws_client(reg: &str) -> Result<Client, Box<dyn std::error::Error>> {
+    let key_id = env::var("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID must be set");
+    let key_secret = env::var("AWS_ACCESS_KEY_SECRET").expect("AWS_ACCESS_KEY_SECRET must be set");
+
+    let cred = Credentials::new(key_id, key_secret, None, None, "loaded-from-custom-env");
+
+    let region = Region::new(reg.to_string());
+    let conf_builder = Builder::new().region(region).credentials_provider(cred);
+    let conf = conf_builder.build();
+
+    let client = Client::from_conf(conf);
+
+    Ok(client)
 }

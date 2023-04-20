3a4711d4d5da8f6015f97537e162e31c1b6fab38
João Ferreira <jbrazf.01@gmail.com>
2023-04-20 01:38:24 +0100

    pred ms is containerizes

--- src/prediction/flask-app/app.py
+++
@@ -1,7 +0,0 @@
-import json
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-
-@app.route('/flights/airlines', methods=['GET'])
--- src/prediction/prediction-ms/src/main.rs
+++ src/prediction/prediction-ms/src/main.rs
@@ -23,7 +23,7 @@ struct AWSArgs {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(liveness).service(predict))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
